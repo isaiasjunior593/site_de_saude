525f80e3dddf9e783f8b994b3eddc616173be079
isaiasjunior593 <161721526+isaiasjunior593@users.noreply.github.com>
2025-03-22 11:10:10 -0300

    Add files via upload

---
+++ site de saude/agua/agua.js
@@ -0,0 +1,25 @@
+const formAgua = document.getElementById('form-agua');
+const litros = document.getElementById('litros');
+
+formAgua.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const peso = parseFloat(document.getElementById('peso').value);
+    const atividade = document.getElementById('atividade').value;
+
+    let litrosDiarios = 0;
+
+    if (atividade === 'leve') {
+        litrosDiarios = peso * 0.035;
+    } else if (atividade === 'moderado') {
+        litrosDiarios = peso * 0.040;
+    } else if (atividade === 'intenso') {
+        litrosDiarios = peso * 0.045;
+    }
+
+    litros.textContent = `${litrosDiarios.toFixed(1)} litros`;
+});
+
+document.getElementById("btn-home").addEventListener("click", function() {
+    window.location.href = "/index.html";
+});
---
+++ site de saude/calorias/calorias.js
@@ -0,0 +1,34 @@
+const formCalorias = document.getElementById('form-calorias');
+const calorias = document.getElementById('calorias');
+
+formCalorias.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const sexo = document.getElementById('sexo').value;
+    const idade = parseInt(document.getElementById('idade').value);
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    const atividade = document.getElementById('atividade').value;
+
+    let caloriasDiarias = 0;
+
+    if (sexo === 'masculino') {
+        caloriasDiarias = (10 * peso) + (6.25 * altura) - (5 * idade) + 5;
+    } else if (sexo === 'feminino') {
+        caloriasDiarias = (10 * peso) + (6.25 * altura) - (5 * idade) - 161;
+    }
+
+    if (atividade === 'leve') {
+        caloriasDiarias *= 1.2;
+    } else if (atividade === 'moderado') {
+        caloriasDiarias *= 1.55;
+    } else if (atividade === 'intenso') {
+        caloriasDiarias *= 1.9;
+    }
+
+    calorias.textContent = `${caloriasDiarias.toFixed(0)} calorias`;
+});
+
+document.getElementById("btn-home").addEventListener("click", function() {
+    window.location.href = "/index.html";
+});
